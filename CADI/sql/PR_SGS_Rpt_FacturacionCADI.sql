9dc0b9afdeaa48d287ee39fe9515c09dcdc74773
John Lopez <johnalbh@gmail.com>
2016-11-16 10:42:09 -0500

    Ajustes Consultas

--- CADI/sql/PR_SGS_Rpt_FacturacionCADI.sql
+++ CADI/sql/PR_SGS_Rpt_FacturacionCADI.sql
@@ -18,37 +18,26 @@ REQUERIMIENTO:
 EMPRESA:
 FECHA MODIFICACIÓN:
 ********************************************************************/
-CREATE PROCEDURE 
+ALTER PROCEDURE 
 	 [dbo].[PR_SGS_Rpt_FacturacionCADI] 
 	 
-		 @TipoIdentificacion VARCHAR(30)
-		,@NumeroIdentificacion VARCHAR (30) 
-		,@FechaInicio DATETIME
+         @FechaInicio DATETIME
 		,@FechaFin DATETIME
-		,@Especialidad VARCHAR (30)
+
 AS
 BEGIN
-DECLARE @Especialidades TABLE 
-	(
-		IdEspecialidad VARCHAR(60)
-	)
-INSERT INTO @Especialidades
-
-SELECT ES.Valor AS Especialidad
-FROM F_SGS_Split(@Especialidad, ',') AS ES
-
 
 SELECT 
 
-CONVERT (VARCHAR(10),NVM.Fecha1,110) AS FechaSesion
-,PR.PrimerApellido + ' ' + isNull(PR.SegundoApellido,'') + ' ' + PR.PrimerNombre + ' ' + isNull(PR.SegundoNombre,'')  AS NombrePaciente
-,[dbo].[F_SGS_CalcularEdadAniosMeses] (PR.FechaNacimiento) AS Edad
-,NVM.NombreNovedad AS NombreNovedad
-,TNM.Nombre	AS NombreTipoNovedad
-,NVM.ValorDominio AS Especialidad
-,NVM.Descripcion AS Descripcion
+
+ PR.PrimerApellido + ' ' + isNull(PR.SegundoApellido,'') + ' ' + PR.PrimerNombre + ' ' + isNull(PR.SegundoNombre,'')  AS NombrePaciente
 ,CR.Nombre AS Curso
-,MAT.Nombre AS Materia 
+,EST.CodigoEstudiante AS Código
+,COUNT (*) As Programdas
+,SUM (IIF (NVM.Checkbox1 IS NULL, 1 , 1- NVM.Checkbox1)) AS Asistencias
+,SUM (IIF (NVM.Checkbox6 IS NULL OR NVM.Checkbox6 = 0 , 1, 0 ))  As SesionesFacturar
+,SUM (IIF (NVM.Checkbox6 IS NULL OR NVM.Checkbox6 = 0 , CONVERT (INT, DOM.Descripcion), 0)) AS ValorAFacturar
+
 
 FROM NovedadMedica  AS NVM
 INNER JOIN PersonaNovedadMedica AS PNM 
@@ -56,7 +45,11 @@ ON NVM.IdNovedadMedica = PNM.IdNovedadMedica
 
 INNER JOIN Persona AS PR
 ON PNM.TipoIdentificacionPersona = PR.TipoIdentificacion 
-AND PNM.NumeroIdentificacionPersona = PR.NumeroIdentificacion
+AND PNM.NumeroIdentificacionPersona = PR.NumeroIdentificacion 
+
+INNER JOIN ESTUDIANTE AS EST
+ON EST.TipoIdentificacion = PNM.TipoIdentificacionPersona
+AND EST.NumeroIdentificacion = PNM.NumeroIdentificacionPersona
 
 INNER JOIN EstudianteCurso AS ES 
 ON PNM.TipoIdentificacionPersona = ES.TipoIdentificacionEstudiante
@@ -65,12 +58,6 @@ AND ES.Estado <> 'Retirado'
 
 INNER JOIN Curso as CR
 ON ES.IdCurso = CR.IdCurso 
-
-INNER JOIN Nivel as NV
-ON CR.IdNivel = NV.IdNivel
-
-INNER JOIN Seccion AS SE
-ON NV.IdSeccion = SE.IdSeccion
 
 INNER JOIN PeriodoLectivo AS PL
 ON CR.AnioAcademico = PL.ID
@@ -82,53 +69,20 @@ ON NVM.IdTipoNovedad = TNM.IdTipoNovedadMedica
 INNER JOIN TipoNovedadMedicaPestania AS TNMP
 ON TNM.IdTipoNovedadMedica = TNMP.IdTipoNovedadMedica
 
-INNER JOIN Usuario AS USR
-ON NvM.UsuarioLog = USR.mail
-
-LEFT JOIN CALENDARIO AS CAL 
-ON NVM.FECHA1=CAL.FECHA
-
-LEFT JOIN TipoHorario AS TH
-	ON TH.IdTipoHorario = CASE 
-					WHEN SE.IdSeccion = 1 THEN CAL.TipoHorarioPreescolar 
-					WHEN SE.IdSeccion = 2 THEN CAL.TipoHorarioPrimaria
-					WHEN SE.IdSeccion = 3 THEN CAL.TipoHorarioBachillerato
-				END 
-	AND  CONVERT(VARCHAR(50),TH.NumeroHora)= NVM.CampoTexto4
-
-LEFT JOIN Horario AS HO
-	ON TH.NumeroHora = HO.Hora 
-	AND HO.IdCurso = CR.IdCurso 
-	AND HO.Dia = cal.NumeroDia
-
-LEFT JOIN Materia AS MAT
-	ON HO.IdMateria = MAT.Id
-
 INNER JOIN DOMINIO DOM 
 	ON NVM.ValorDominio = DOM.Valor
-
+	AND DOM.Dominio = 'TarifaCADI'
 
 WHERE 
-	TNMP.IdPestaniaDashboard =  9 AND TN
-	AND PNM.TipoIdentificacionPersona = @TipoIdentificacion
-	AND PNM.NumeroIdentificacionPersona = @NumeroIdentificacion
+	TNMP.IdPestaniaDashboard =  9 AND TNMP.IdTipoNovedadMedica in ('37','40') 
 	AND NVM.Fecha1 BETWEEN @FechaInicio AND @FechaFin
-	AND NVM.ValorDominio = @Especialidad
+
+GROUP BY 
+	EST.CodigoEstudiante
+   ,PR.PrimerApellido + ' ' + isNull(PR.SegundoApellido,'') + ' ' + PR.PrimerNombre + ' ' + isNull(PR.SegundoNombre,'')
+   ,CR.Nombre
 
 END
 GO
 
 
-SELECT * FROM NOVEDADMEDICA
-SELECT * FROM TipoNovedadMedicaPestania
-SELECT * FROM Dominio
-
-EspecialidadCADI	Fono
-EspecialidadCADI	Psico
-EspecialidadCADI	TO
-
-TarifaCADI	'Fono'
-TarifaCADI	'Psico'
-TarifaCADI	'To'
-
-SELECT 
